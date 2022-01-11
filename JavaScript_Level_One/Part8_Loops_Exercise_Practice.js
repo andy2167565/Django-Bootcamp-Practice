4c101e55aaa1e7a6fb4a7b9e2a49d3f6f8866a35
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-01-11 22:01:01 +0800

    Create Part8_Loops_Exercise_Practice.js

---
+++ JavaScript_Level_One/Part8_Loops_Exercise_Practice.js
@@ -0,0 +1,30 @@
+// Problem 1
+// While Loop
+var i = 0;
+while (i < 5) {
+  console.log("Hello");
+  i++;
+}
+
+// For Loop
+for (var i = 0; i < 5; i++) {
+  console.log("Hello");
+}
+
+
+// Problem 2
+// While Loop
+var i = 1
+while (i < 26) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+  i++;
+}
+
+// For Loop
+for (var i = 1; i < 26; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+}
