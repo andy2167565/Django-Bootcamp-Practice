544ada9b4ec4bbf9554f8b40c15a3976d3ca9eb2
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-02-08 02:01:36 +0800

    Add files via upload

---
+++ 14-Django_Level_One/ProTwo_Practice/AppTwo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    help_dict = {'insert_help': 'HELP PAGE'}
+    return render(request, 'AppTwo/help.html', context=help_dict)
